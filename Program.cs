8389c4ce0c2b14896dd59c4e15051a11e794ff2c
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-13 18:57:15 +0400

    Task1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Task 1
+static double CalculateFormula (int a , int b , int c , int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result ; 
+}
+System.Console.WriteLine(CalculateFormula(2,3,4,5));
+
+
